be9d129ee915e12b98dd9ebc8699c6e536aa4c81
p0daru <102858022+p0daru@users.noreply.github.com>
2024-05-21 23:06:42 +0300

    updates

--- src/js/menu/menu.js
+++ src/js/menu/menu.js
@@ -15,57 +15,85 @@ import readline from 'readline';
 // const result = prompt('hoe');
 // prompt.start();
 
-function menu() {
+async function menu() {
   let isFinished = false;
 
   while (!isFinished) {
     console.log('==========================');
     console.log('ГОЛОВНЕ МЕНЮ');
-    console.log('Welcome! Обери одну з доступних опцій:');
+    console.log('Welcome! Доступні опції:');
     console.log('1. Розв`язати задачу всіма можливими методами');
     console.log('2. Провести експерименти');
-    console.log('3. Вивести дані задачі');
-    console.log('4. Завершити роботу');
+
+    /**
+     * Всередині опції 1
+     * console.log('Обрати спосіб генерування задачі');
+     * console.log('1.2 Згенерувати випадковим чином');
+     * console.log('2.2 Скористатись дефолтними вхідними даними');
+     *
+     */
+
+    console.log('2. Завершити роботу');
     console.log('==========================');
 
     // значення від користувача
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+    // const rl = readline.createInterface({
+    //   input: process.stdin,
+    //   output: process.stdout,
+    // });
 
-    rl.question('Введіть щось: ', answer => {
-      console.log('Ви ввели: ' + answer);
-      rl.close();
-    });
+    // let userInput; // Змінна для зберігання відповіді
 
-    let inputIsCorrect = false;
-    while (!inputIsCorrect) {
-      switch (rl) {
-        case 1:
-          console.log('Розв`язати задачу всіма можливими методами');
-          inputIsCorrect = true;
-          break;
-        case 2:
-          console.log('Провести експерименти');
-          inputIsCorrect = true;
-          break;
-        case 3:
-          console.log('Вивести дані задачі');
-          inputIsCorrect = true;
-          break;
-        case 4:
-          console.log('Завершити роботу');
-          inputIsCorrect = true;
+    // rl.question('Введіть щось: ', answer => {
+    //   console.log('Ви ввели: ' + answer);
+    //   userInput = answer; // Зберігаємо введення у змінну
+    //   rl.close();
+    // });
 
-          break;
-        default:
-          console.log('Incorrect input. Try again!');
-          inputIsCorrect = false;
-      }
+    let option = await askQuestion('Ввести опцію: ');
+
+    // let inputIsCorrect = false;
+    switch (option) {
+      case 1:
+        console.log('Розв`язати задачу всіма можливими методами');
+        // inputIsCorrect = true;
+        break;
+      case 2:
+        console.log('Провести експерименти');
+        // inputIsCorrect = true;
+        break;
+      case 3:
+        console.log('Вивести дані задачі');
+        // inputIsCorrect = true;
+        break;
+      case 4:
+        console.log('Завершити роботу');
+        // inputIsCorrect = true;
+        break;
+      default:
+        console.log('Incorrect input. Try again!');
+      // inputIsCorrect = false;
     }
-    isFinished = true;
+
+    // опрацювання некоректного інпуту (наприклад, введено опцію 7)
+    // while (!inputIsCorrect) {
+    //   inputIsCorrect = true;
+
+    // }
+    // isFinished = true;
   }
 }
 
+function askQuestion(query) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise(resolve => {
+    rl.question(query, answer => {
+      resolve(answer);
+    });
+  });
+}
 menu();
