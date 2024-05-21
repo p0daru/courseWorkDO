90c9ed4de45cc536a0fb441951c0cf3f95bd71a3
p0daru <102858022+p0daru@users.noreply.github.com>
2024-05-21 09:27:38 +0300

    update ant

--- src/js/algorithms/ant/ant_console.js
+++ src/js/algorithms/ant/ant_console.js
@@ -1,33 +1,8 @@
 // Kovalenko Kateryna
-
 import * as Generator from '../../generator/taskGenerator.js';
 export function ant_console(n, t_matrix, alpha) {
-    // Вимірювання часу виконання алгоритму
-    const startTime = performance.now();
-    // Параметри алгоритму 
-    let beta = 1;
-    let tay_0 = 0.7;
-    let p = 0.1;
-
-    ///
-    let tay_matrix = [];
-    let lMax = 10;
-    let result = [];
-    let result_func;
-    let repr = 0;
-
-    let t_use = t_matrix.slice().map(row => row.slice());
-    //Матриця тривалостей уроків
-    let less_matrix = Generator.generateLessonDuration(n);
-
-    console.log("Тривалості занять:");
-    console.table(less_matrix);
-import { generateMatrix } from '../../generator/taskGenerator.js';
-
-export function ant_console(n, t_matrix, alpha = 1) {
   // Вимірювання часу виконання алгоритму
   const startTime = performance.now();
-
   // Параметри алгоритму
   let beta = 1;
   let tay_0 = 0.7;
@@ -38,8 +13,14 @@ export function ant_console(n, t_matrix, alpha = 1) {
   let lMax = 10;
   let result = [];
   let result_func;
+  let repr = 0;
 
   let t_use = t_matrix.slice().map(row => row.slice());
+  //Матриця тривалостей уроків
+  let less_matrix = Generator.generateLessonDuration(n);
+
+  console.log('Тривалості занять:');
+  console.table(less_matrix);
 
   console.log('Матриця переналаштувань:');
   console.table(t_matrix);
@@ -117,69 +98,6 @@ export function ant_console(n, t_matrix, alpha = 1) {
         tay_matrix[i][j] = Infinity;
       }
     }
-
-    console.log("Матриця ферамонів:");
-    console.table(tay_matrix); // Основна частина програми
-    for (let k = 1; k <= lMax; k++) {
-        let student = Math.floor(Math.random() * n); // Довільним чином обираємо першого учня на кожній ітерації
-        console.log("ІТЕРАЦІЯ", k)
-        console.log("Перший учень: ", student);
-        let iteration_matrix = t_matrix.slice().map(row => row.slice());
-        let result_it = []; // змінна з результатами кожної ітерації
-        let result_it_func = 0;
-        let repr_it = 0;
-        let visited_array = Array.from({ length: n }, () => Array(n).fill(0));
-        result_it.push(student);
-        while (result_it.length < n) {
-            for (let x = 0; x < n; x++)
-                iteration_matrix[x][student] = Infinity;
-            // console.log(iteration_matrix);
-            // Обчислення знаменника з формули 1
-            let formula_denominator = 0;
-            let res_set = new Map();
-            for (let i = 0; i < n; i++) {
-                if (iteration_matrix[student][i] != Infinity) {
-                    formula_denominator += parseFloat((tay_matrix[student][i]) ** alpha) * ((1 / t_matrix[student][i]) ** beta);
-                }
-            }
-            for (let i = 0; i < n; i++) {
-                let it = 0;
-                if (iteration_matrix[student][i] != Infinity) {
-                    it = parseFloat(((tay_matrix[student][i]) ** alpha) * ((1 / t_matrix[student][i]) ** beta) / formula_denominator).toFixed(4);
-                    if (res_set.size === 0)
-                        res_set.set(i, parseFloat(it));
-                    else {
-                        let lastValue = Array.from(res_set.values()).pop();
-                        it = parseFloat(parseFloat(it) + parseFloat(lastValue)).toFixed(4);
-                        res_set.set(i, parseFloat(it));
-                    }
-                }
-            }
-
-            console.log("Проміжний результат: ", res_set);
-            let randomNumber = parseFloat(Math.random()).toFixed(4); // Генерує випадкове число від 0 до 1 
-            console.log("Згенероване число: ", randomNumber);
-
-            for (let [key, value] of res_set.entries()) {
-                if (parseFloat(randomNumber) <= parseFloat(value)) {
-                    student = key;
-                    result_it.push(student);
-                    break;
-                }
-            }
-
-            console.log("Наступний учень : ", student);
-            console.log("Поточний розклад: ", result_it);
-            console.log();
-        }
-        // Обчислення ЦФ :
-
-        for (let i = 0; i < result_it.length - 1; i++) {
-            const currentStudent = result_it[i];
-            const nextStudent = result_it[i + 1];
-            result_it_func += parseFloat(t_matrix[currentStudent][nextStudent]) + less_matrix[i];
-            repr_it += parseFloat(t_matrix[currentStudent][nextStudent]);
-            visited_array[currentStudent][nextStudent] = 1;
   }
 
   console.log('Матриця ферамонів:');
@@ -191,6 +109,7 @@ export function ant_console(n, t_matrix, alpha = 1) {
     let iteration_matrix = t_matrix.slice().map(row => row.slice());
     let result_it = []; // змінна з результатами кожної ітерації
     let result_it_func = 0;
+    let repr_it = 0;
     let visited_array = Array.from({ length: n }, () => Array(n).fill(0));
     result_it.push(student);
     while (result_it.length < n) {
@@ -233,17 +152,6 @@ export function ant_console(n, t_matrix, alpha = 1) {
           result_it.push(student);
           break;
         }
-
-        console.log("Матриця феромонів: ");
-        console.table(tay_matrix);
-        if (k === 1) {
-            result = result_it;
-            result_func = result_it_func;
-            repr = repr_it;
-        } else if (result_func > result_it_func) {
-            result = result_it;
-            result_func = result_it_func;
-            repr = repr_it;
       }
 
       console.log('Наступний учень : ', student);
@@ -251,10 +159,13 @@ export function ant_console(n, t_matrix, alpha = 1) {
       console.log();
     }
     // Обчислення ЦФ :
+
     for (let i = 0; i < result_it.length - 1; i++) {
       const currentStudent = result_it[i];
       const nextStudent = result_it[i + 1];
-      result_it_func += parseFloat(t_matrix[currentStudent][nextStudent]);
+      result_it_func +=
+        parseFloat(t_matrix[currentStudent][nextStudent]) + less_matrix[i];
+      repr_it += parseFloat(t_matrix[currentStudent][nextStudent]);
       visited_array[currentStudent][nextStudent] = 1;
     }
     console.table(t_matrix);
@@ -278,58 +189,35 @@ export function ant_console(n, t_matrix, alpha = 1) {
         }
       }
     }
-
-    //////
-    const endTime = performance.now();
-    const executionTime = endTime - startTime;
-    console.log();
-    console.log("------------------------------------------------------");
-    console.log("Кінцевий результат: ", result);
-    console.log("Час переналаштувань: ", repr);
-    console.log("Значення ЦФ: ", result_func);
-    console.log("ExecutionTime: ", executionTime);
-
-
-    return { result, result_func, executionTime };
     console.log('Матриця феромонів: ');
     console.table(tay_matrix);
     if (k === 1) {
       result = result_it;
       result_func = result_it_func;
+      repr = repr_it;
     } else if (result_func > result_it_func) {
       result = result_it;
       result_func = result_it_func;
+      repr = repr_it;
     }
   }
-  console.log();
-  console.log('------------------------------------------------------');
-  console.log('Кінцевий результат: ', result);
-  console.log('Значення ЦФ: ', result_func);
   //////
   const endTime = performance.now();
   const executionTime = endTime - startTime;
+  console.log();
+  console.log('------------------------------------------------------');
+  console.log('Кінцевий результат: ', result);
+  console.log('Час переналаштувань: ', repr);
+  console.log('Значення ЦФ: ', result_func);
+  console.log('ExecutionTime: ', executionTime);
 
   return { result, result_func, executionTime };
 }
 
 // Test Case
 try {
-    // let t_matrix = generateMatrix(8, 10, 5);
-
-    const n = 8;
-    let t_matrix = [
-        [Infinity, 15, Infinity, 20, Infinity, 5, Infinity, 10],
-        [5, Infinity, 15, Infinity, 5, Infinity, 10, Infinity],
-        [Infinity, 20, Infinity, 15, Infinity, 5, Infinity, 5],
-        [10, Infinity, 20, Infinity, 5, Infinity, 10, Infinity],
-        [Infinity, 15, Infinity, 15, Infinity, 5, Infinity, 5],
-        [10, Infinity, 15, Infinity, 10, Infinity, 10, Infinity],
-        [Infinity, 20, Infinity, 20, Infinity, 10, Infinity, 5],
-        [5, Infinity, 15, Infinity, 5, Infinity, 5, Infinity]
-    ];
-    let alpha = 1;
-    ant_console(n, t_matrix, alpha);
   // let t_matrix = generateMatrix(8, 10, 5);
+
   const n = 8;
   let t_matrix = [
     [Infinity, 15, Infinity, 20, Infinity, 5, Infinity, 10],
